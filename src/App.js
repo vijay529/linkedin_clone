afef8a23f4a56ac18d454ab71a14353ccbdb2664
unknown <vijaychoudhary6017@gmail.com>
2023-12-17 11:06:15 +0530

    app js change

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home';
 import Header from './components/Header';
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
+import 'dotenv/config'
 
 function App(props) {
   useEffect(() => {
